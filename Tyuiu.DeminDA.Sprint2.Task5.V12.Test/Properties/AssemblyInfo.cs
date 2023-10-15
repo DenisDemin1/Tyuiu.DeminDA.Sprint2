54bdcb7995728cb5fcb370569a62b4be72001a21
DeminDenis1 <maxwelldenis888@gmail.com>
2023-10-15 19:34:59 +0500

    Task5

---
+++ Tyuiu.DeminDA.Sprint2.Task5.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DeminDA.Sprint2.Task5.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DeminDA.Sprint2.Task5.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d74b2932-af30-4945-99be-e988a41a3552")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DeminDA.Sprint2.Task5.V12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.DeminDA.Sprint2.Task5.V12.Lib;
+
+namespace Tyuiu.DeminDA.Sprint2.Task5.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            int m = Convert.ToInt32(Console.ReadLine());
+            int g = Convert.ToInt32(Console.ReadLine());
+
+            string res = Convert.ToString(ds.FindDateOfPreviousDay(g, m, n));
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
